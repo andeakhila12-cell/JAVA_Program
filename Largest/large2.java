c86cf2419797ebad3a6f77a4966b43dd5ea569fe
ANDE AKHILA <andeakhila12@gmail.com>
2026-06-14 20:55:40 +0530

    Create large2.java

---
+++ Largest/large2.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class large2 {
+ public static void main(String[] args) {
+	 int a=20;
+	 int b=10;
+	 int c=120;
+	 if(a>b && a>c){
+		 System.out.println(a);
+	 }else if(b>a && b>c) {
+		 System.out.println(b);
+	 }
+	 else System.out.println(c);
+ }
+}
