08c1fb7cbe366e4d5732c99f4942fd3aa06b81db
ANDE AKHILA <andeakhila12@gmail.com>
2026-06-14 20:42:27 +0530

    Create swap.java

---
+++ Basics/swap.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class swap {
+ public static void main(String[] args) {
+	 int a=5;
+	 int b=20;
+	 System.out.println("Before Swap:"+a+" "+b);
+	 int temp;
+	 temp=a;
+	 a=b;
+	 b=temp;
+	 System.out.println("After Swap:"+a+" "+b);
+ }
+}
