959ba58c84448676a3d9bebbacc14ad1e438b317
ANDE AKHILA <andeakhila12@gmail.com>
2026-06-16 20:16:22 +0530

    Create nat.java

---
+++ Basics/nat.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class nat {
+ public static void main(String[] args) {
+	 int sum=0;
+	 for(int i=0;i<=10;i++) {
+		 sum=sum+i;
+	 }
+	 System.out.println("Sum of 1st 10 natural numbers:"+sum);
+ }
+}
