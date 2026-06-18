a584e0b04c984ad5a8fe3a70c350bca3a0188cfe
ANDE AKHILA <andeakhila12@gmail.com>
2026-06-18 13:10:43 +0530

    Create largearr.java

---
+++ Basics/largearr.java
@@ -0,0 +1,23 @@
+package basic;
+import java.util.Scanner;
+public class largearr {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter size of array");
+		int n=sc.nextInt();
+		System.out.println("Enter the array elements:");
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int max=arr[0];
+		for(int i=1;i<n;i++) {
+			if(arr[i]>max) {
+				max=arr[i];
+			}
+		}
+		System.out.println("Largest number: "+max);
+		sc.close();
+	}
+
+}
