2d37439f175bd464bcaa22822129f2cbd215bb7b
ANDE AKHILA <andeakhila12@gmail.com>
2026-07-01 15:43:56 +0530

    Create multiple.java

---
+++ Real _Time/multiple.java
@@ -0,0 +1,16 @@
+package arr;
+
+public class multiple {
+	
+		public static void main(String[] args) {
+			
+			int num=10;
+			if(num%3==0 && num%5==0) System.out.println("Zoom");
+			else if(num%3==0) System.out.println("Zip");
+			else if(num%5==0) System.out.println("Zap");
+			else System.out.println("Invalid");
+		}
+	}
+
+
+
